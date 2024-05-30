7bf20774f7e079b3bce55463e9d2c62b5cc6e213
Yooinhak <inhak.yoo@corretto.io>
2024-05-30 18:01:17 +0900

    fix: eslint

--- src/app/playground.tsx
+++ src/app/playground.tsx
@@ -1,13 +1,20 @@
+import { useEffect } from 'react';
+
 import Button from '@components/Button';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 
-export default function Home() {
+import Test from './test';
+
+const Component = () => {
   const router = useRouter();
+
+  useEffect(() => {}, []);
 
   return (
     <div>
       <Button />
     </div>
   );
-}
+};
+
+export default Component;
