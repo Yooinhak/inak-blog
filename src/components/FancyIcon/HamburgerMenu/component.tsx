542fa02ae64baf992a20b9e960022cb8bfa17d0b
Yooinhak <inhak.yoo@corretto.io>
2024-07-12 16:31:15 +0900

    fix. disclosure

--- src/components/FancyIcon/HamburgerMenu/component.tsx
+++ src/components/FancyIcon/HamburgerMenu/component.tsx
@@ -1,41 +1,24 @@
-'use client';
+const Component = () => {
+  const defaultStyle =
+    'block h-1 bg-blue-500 rounded transition-all duration-400 ease-in-out absolute';
 
-import { useState } from 'react';
-
-interface props {
-  isCheck: boolean;
-  setIsCheck: (value: boolean) => void;
-}
-
-const Component = ({ isCheck, setIsCheck }: props) => {
   return (
-    <div className="inline-block">
-      <input
-        type="checkbox"
-        id="checkbox"
-        className="absolute hidden"
-        checked={isCheck}
-        onChange={() => setIsCheck(!isCheck)}
-      />
-      <label htmlFor="checkbox" className="cursor-pointer">
-        <div className="w-8 h-8 relative">
-          <span
-            className={`block w-8 h-1 bg-blue-500 rounded transition-all duration-400 ease-in-out absolute ${isCheck ? 'top-3.5 bg-transparent' : 'top-0'}`}
-          ></span>
-          <span
-            className={`block h-1 bg-blue-500 rounded transition-all duration-400 ease-in-out absolute top-3.5 ${isCheck ? 'w-8 transform rotate-45 left-0' : 'w-1 rotate-90 left-3.5'}`}
-          ></span>
-          <span
-            className={`block h-1 bg-blue-500 rounded transition-all duration-400 ease-in-out absolute top-3.5 right-0 ${isCheck ? 'w-8 -rotate-45 left-0' : 'w-1 left-3.5'}`}
-          ></span>
-          <span
-            className={`block w-8 h-1 bg-blue-500 rounded transition-all duration-400 ease-in-out absolute bottom-0 top-3.5 ${isCheck ? 'bottom-3.5 bg-transparent' : 'bottom-0'}`}
-          ></span>
-          <span
-            className={`block w-8 h-1 bg-blue-500 rounded transition-all duration-400 ease-in-out absolute bottom-0 ${isCheck ? 'bottom-3.5 bg-transparent' : 'bottom-0'}`}
-          ></span>
-        </div>
-      </label>
+    <div className="w-8 h-8 relative">
+      <span
+        className={`${defaultStyle} w-8 top-0 group-data-[open]:top-3.5 group-data-[open]:bg-transparent`}
+      ></span>
+      <span
+        className={`${defaultStyle} w-1 top-3.5 rotate-90 left-3.5 group-data-[open]:w-8 group-data-[open]:transform group-data-[open]:rotate-45 group-data-[open]:left-0`}
+      ></span>
+      <span
+        className={`${defaultStyle} w-1 left-3.5 top-3.5 right-0 group-data-[open]:w-8 group-data-[open]:-rotate-45 group-data-[open]:left-0`}
+      ></span>
+      <span
+        className={`${defaultStyle} w-8 bottom-0 top-3.5 group-data-[open]:bottom-3.5 group-data-[open]:bg-transparent`}
+      ></span>
+      <span
+        className={`${defaultStyle} w-8 bottom-0 group-data-[open]:bottom-3.5 group-data-[open]:bg-transparent`}
+      ></span>
     </div>
   );
 };
--- src/components/Header/component.tsx
+++ src/components/Header/component.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import {
   Disclosure,
   DisclosureButton,
@@ -10,11 +8,8 @@ import { useState } from 'react';
 import HamburgerMenu from '@components/FancyIcon/HamburgerMenu';
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+  { name: 'Posts', href: '#', current: true },
+  { name: 'About', href: '#', current: false },
 ];
 
 function classNames(...classes: string[]) {
@@ -22,107 +17,73 @@ function classNames(...classes: string[]) {
 }
 
 const Component = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
   return (
-    <>
-      <div className="min-h-full">
-        <Disclosure as="nav" className="bg-gray-800">
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 items-center justify-between">
-              <div className="flex items-center">
-                <div className="flex-shrink-0">
-                  <img
-                    alt="Your Company"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    className="h-8 w-8"
-                  />
-                </div>
+    <Disclosure as="nav" className="bg-gray-800">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex h-16 items-center justify-between">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <img
+                alt="Your Company"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                className="h-8 w-8"
+              />
+            </div>
 
-                <div className="hidden md:block">
-                  <div className="ml-10 flex items-baseline space-x-4">
-                    {navigation.map(item => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        aria-current={item.current ? 'page' : undefined}
-                        className={classNames(
-                          item.current
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium',
-                        )}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-
-              <div className="hidden md:block">
-                <div className="ml-4 flex items-center md:ml-6">
-                  <button
-                    type="button"
-                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
+                {navigation.map(item => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? 'page' : undefined}
+                    className={classNames(
+                      item.current
+                        ? 'bg-gray-900 text-white'
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'rounded-md px-3 py-2 text-sm font-medium',
+                    )}
                   >
-                    <span className="absolute -inset-1.5" />
-                    <span className="sr-only">View notifications</span>
-                    {/* <BellIcon aria-hidden="true" className="h-6 w-6" /> */}
-                  </button>
-                </div>
-              </div>
-              <div className="-mr-2 flex md:hidden">
-                {/* Mobile menu button */}
-                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  {/* <span className="absolute -inset-0.5" /> */}
-                  {/* <span className="sr-only">Open main menu</span> */}
-                  <HamburgerMenu
-                    isCheck={isMenuOpen}
-                    setIsCheck={setIsMenuOpen}
-                  />
-                  {/* <Bars3Icon
-                    aria-hidden="true"
-                    className="block h-6 w-6 group-data-[open]:hidden"
-                  />
-                  <XMarkIcon
-                    aria-hidden="true"
-                    className="hidden h-6 w-6 group-data-[open]:block"
-                  /> */}
-                </DisclosureButton>
+                    {item.name}
+                  </a>
+                ))}
               </div>
             </div>
           </div>
 
-          <DisclosurePanel className="md:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-              {navigation.map(item => (
-                <DisclosureButton
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  aria-current={item.current ? 'page' : undefined}
-                  className={classNames(
-                    item.current
-                      ? 'bg-gray-900 text-white'
-                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium',
-                  )}
-                >
-                  {item.name}
-                </DisclosureButton>
-              ))}
-            </div>
-          </DisclosurePanel>
-        </Disclosure>
-
-        <main>
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            {/* Your content */}
+          <div className="-mr-2 flex md:hidden">
+            {/* Mobile menu button */}
+            <DisclosureButton className="group">
+              <HamburgerMenu />
+            </DisclosureButton>
           </div>
-        </main>
+        </div>
       </div>
-    </>
+
+      <DisclosurePanel
+        transition
+        className="md:hidden origin-top transition duration-200 ease-out data-[closed]:-translate-y-6 data-[closed]:opacity-0"
+      >
+        <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+          {navigation.map(item => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                item.current
+                  ? 'bg-gray-900 text-white'
+                  : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'block rounded-md px-3 py-2 text-base font-medium',
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
   );
 };
 
