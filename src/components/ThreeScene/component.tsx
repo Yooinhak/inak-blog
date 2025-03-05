92f413086a6b80ef209487998e6ed24c307a2b3e
Yooinhak <syu3236@gmail.com>
2025-03-05 16:59:32 +0900

    fix. canvas size

--- src/components/ThreeScene/component.tsx
+++ src/components/ThreeScene/component.tsx
@@ -12,14 +12,20 @@ const Model = () => {
 
 export const Component = () => {
   return (
-    <Canvas camera={{ position: [0, 2, 5], fov: 50 }} gl={{ alpha: true }}>
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[2, 5, 2]} intensity={1} />
-      <Suspense fallback={null}>
-        <Model />
-      </Suspense>
-      <OrbitControls />
-    </Canvas>
+    <div className="w-full h-[50vh] md:h-[60vh] lg:h-[70vh] xl:h-[80vh]">
+      <Canvas
+        className="w-full h-full"
+        camera={{ position: [0, 2, 5], fov: 50 }}
+        gl={{ alpha: true }}
+      >
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </div>
   );
 };
 
