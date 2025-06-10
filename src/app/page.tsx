5a4779e20118409cd84f226bdd66d21b08103ba5
Yooinhak <syu3236@gmail.com>
2025-06-10 11:09:35 +0900

    feat. rotating text test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
+import RotatingText from '@components/RotatingText';
+
 export default function Main() {
   return (
     <div>
+      <RotatingText
+        texts={['React', 'Bits', 'Is', 'Cool!']}
+        mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+        staggerFrom={'last'}
+        initial={{ y: '100%' }}
+        animate={{ y: 0 }}
+        exit={{ y: '-120%' }}
+        staggerDuration={0.025}
+        splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+        transition={{ type: 'spring', damping: 30, stiffness: 400 }}
+        rotationInterval={2000}
+      />
       <div className="card bg-base-100 w-96 shadow-sm">
         <figure>
           <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
